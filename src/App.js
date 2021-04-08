51eec635175afbb78f8501d52d8db09c0044fbf0
dp160610 <46530061+dp160610@users.noreply.github.com>
2021-04-08 15:32:17 +0530

    Responsive Updates

--- src/App.js
+++ src/App.js
@@ -1,74 +1,59 @@
-import React, { Component } from 'react';
-import Announcement from './AnnouncementBar/AnnouncementBar';
-import TopBar from './TopBar/TopBar';
-import Home from './Home/Home';
-import About from './About/About';
-import Shop from './Shop/Shop';
-import Donate from './Donate/Donate';
-import Contact from './Contact/Contact';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import CovidAlert from './CovidAlert/CovidAlert';
-import ProductDetailsPage from './ProductDetailsPage/ProductDetailsPage';
-import CartDetailsSlide from './CartDetailsSlide/CartDetailsSlide';
-import OrderConfirmationPage from './OrderConfirmationPage/OrderConfirmationPage';
-import { connect } from 'react-redux';
-import { cartOpenCloseAction } from './actions/actions';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {useDispatch} from 'react-redux'
+import Home from './Page-components/Home/Home'
+import About from './Page-components/About/About'
+import Shop from './Page-components/Shop/Shop'
+import Donate from './Page-components/Donate/Donate'
+import Contact from './Page-components/Contact/Contact'
+import Cart from './Page-components/components/Cart'
+import {useEffect} from 'react'
+import {addProduct} from './Action/action'
+import ProductDetail from './Page-components/Shop/ProductDetail';
+import { withRouter, useLocation } from "react-router-dom";
+import ReadOurStatement from './Page-components/Home/ReadOurStatement'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-     }
-  }
-
-
-  displayCart = () => {
-    this.props.cartOpenClose();
-  }
-
-
-
-
-
-
-
-  render() {
-    return ( 
-      <BrowserRouter className="main_container">
-        {
-          this.props?.cartOpen ?
-            <div  onClick={this.displayCart} className="cart_content_container">
-              <CartDetailsSlide close={this.displayCart} />
-            </div>
-            :
-            ""
-        }
-        <div className="top_bar_wrapper">
-          <Announcement />
-          <TopBar onClick={this.displayCart} />
-        </div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/shop" component={Shop} />
-          <Route path="/donate" component={Donate} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/covid_alert" component={CovidAlert} />
-          <Route path="/product_details_page" component={ProductDetailsPage} />
-          <Route path="/order_confirmation_page" component={OrderConfirmationPage} />
-        </Switch>
-      </BrowserRouter>
-     );
-  }
-}
- 
-const mapStateToProps = (globalStore) => ({
-  cartOpen: globalStore?.productReducer?.cartOpen
-})
-
-const mapDispatchToProps = {
-  cartOpenClose: ()=>cartOpenCloseAction()
+function _ScrollToTop(props) {
+  const { pathname } = useLocation();
+  useEffect(() => {
+      window.scrollTo(0, 0);
+  }, [pathname]);
+  return props.children
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)( App );
+const  ScrollToTop = withRouter(_ScrollToTop)
+
+//Folders are structured as Page components  and common components, reduces, actions, store
+//common components are used accross multiple pages
+  
+
+function App() {
+  let dispatch = useDispatch()
+  
+  useEffect(() =>{
+    dispatch(addProduct())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+  return (
+    <div>
+      <Cart  />
+      <Router>
+        <ScrollToTop>
+          <Switch>
+            <Route exact path={`/`}  component={Home}/>
+            <Route path={`/about`}  component={About}/>
+            <Route exact path={`/shop`}  component={Shop}/>
+            <Route  path={`/shop/product/:id`} component={ProductDetail}/>
+            <Route exact path={`/donate`}  component={Donate}/>
+            <Route path={`/donate/:id`} component={ProductDetail}/>
+            <Route path={`/contact`}  component={Contact}/>
+            <Route path={`/alert/how-were-responding-to-covid-19`}  component={ReadOurStatement} />
+            <Route render = {()=><h1>400 ERROR</h1>} />
+          </Switch>
+        </ScrollToTop>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
